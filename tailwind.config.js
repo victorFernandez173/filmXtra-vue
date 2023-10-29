fe3b3a0718ec9726d86b49dd334b50c090ced822
victorFernandez173 <victor.fer.gor@proton.me>
2023-10-29 15:41:41 +0100

    preparadas ciertas configuraciones

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,31 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.vue',
+        "./node_modules/flowbite/**/*.js",
     ],
 
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                oswald: ['Oswald'],
+            },
+            colors: {
+                'flamingo': '#e37f81',
+            },
+            borderWidth: {
+                DEFAULT: '1px',
+                '0': '0',
+                '2': '2px',
+                '3': '3px',
+                '4': '4px',
+                '6': '6px',
+                '8': '8px',
             },
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        forms,
+    ],
 };
