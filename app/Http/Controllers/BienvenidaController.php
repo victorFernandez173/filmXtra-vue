cdceb06ddad79c367e06cbfa9ea13e9e46cb1193
victorFernandez173 <victor.fer.gor@proton.me>
2023-10-29 15:29:56 +0100

    controlador bienvenida y otros...

---
+++ app/Http/Controllers/BienvenidaController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DB;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Exception;
+
+class BienvenidaController extends Controller
+{
+    /**
+     * Genera un array con 16 ids al azar
+     * @throws Exception
+     */
+    public function obtenerObrasAleatorias(){
+        $numPeliculas = DB::table('obras')->count();
+        $peliculasId = [];
+        for($i = 0; $i < 24; $i++){
+            $aleatorio = rand(1, $numPeliculas);
+            while(in_array($aleatorio, $peliculasId)){
+                $aleatorio = rand(1, $numPeliculas);
+            }
+            $peliculasId[] = $aleatorio;
+        }
+        return $peliculasId;
+    }
+
+    /**
+     * Devuelve la vista de bienvenida con esos 16 id
+     * @throws Exception
+     */
+    public function bienvenida(){
+        return Inertia::render('Welcome', [
+            'obras' => DB::table('obras')->select('obras.titulo', 'p.ruta', 'p.alt')->join('posters AS p', 'obras.id', '=', 'p.obra_id')->whereIn('obras.id', $this->obtenerObrasAleatorias())->get()
+        ]);
+    }
+
+    /**
+     * Devuelve la vista de bienvenida con esos 16 id
+     * @throws Exception
+     */
+    public function buscarTitulo(Request $request){
+
+        $obras = DB::table('obras')->select('obras.titulo', 'p.ruta', 'p.alt')->join('posters AS p', 'obras.id', '=', 'p.obra_id')->where('obras.titulo', 'like', '%' . $request['titulo'] . '%')->get();
+
+        if($request['titulo'] == null || count($obras) == 0){
+            return Inertia::render('Welcome', [
+                'obras' => DB::table('obras')->select('obras.titulo', 'p.ruta', 'p.alt')->join('posters AS p', 'obras.id', '=', 'p.obra_id')->whereIn('obras.id', $this->obtenerObrasAleatorias())->get(),
+                'numResultados' => 0
+            ]);
+        }
+        return Inertia::render('Welcome', [
+            'obras' => $obras,
+            'numResultados' => count($obras)
+        ]);
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,36 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
+use App\Http\Controllers\BienvenidaController;
+
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Rutas web
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| Rutas cargadas para por el RouteServiceProvider desde un grupo que contiene el grupo "web" middleware.
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', [BienvenidaController::class, 'bienvenida'])->name('/');
+
+/*Route::post('/', [BienvenidaController::class, 'buscarTitulo'])->name('/buscado');
+
+Route::get('obra/{titulo}', [ObtenerObraController::class, 'fichaPelicula'])->name('obra');
+
+Route::get('top', [TopObrasController::class, 'cargarDatos'])->name('top');
+
+Route::get('valoraciones', [TopValoracionesController::class, 'cargarDatos'])->name('valoraciones');
+
+Route::get('valoraciones/{titulo}', [FichaValoracionController::class, 'obtenerFichaValoracion'])->name('fichaValoraciones');
+
+Route::post('/like', [LikeController::class, 'darLike'])->name('darLike')->middleware('auth', 'verified');
+
+Route::post('evaluar', [EvaluacionController::class, 'evaluar'])->name('evaluar')->middleware('auth', 'verified');
+
+Route::post('criticar', [CriticaController::class, 'criticar'])->name('criticar')->middleware('auth', 'verified');*/
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -36,3 +39,34 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+//TODO corregir si se puede tema del titulo más pequeño de los poster (length < 50, ver comentario en componente poster); estilos de bienvenida(navbar y footer); estilos de login
+
+// TODO en la pagina de usuario se debería esconder el bloque de contraseña para cuentas o'auth
+// TODO al modificar la ruta de una obra, solo añadiendo caracteres en el trozo del titulo, se intenta acceder a esa obra pero claro, NO EXISTE y da un error de array[0] nulo porque claro no hay obra ESTUDIAR
+// TODO codigo ficha valoraciones, solucionar
+// TODO aplicar video optimización de código de favoritos & optimizaciones de rutas & optimizaciones de consultas??
+// TODO eventserviceprovider event envio mail registros puede ser util?
+// TODO event en RegisteredUserController, implementar para algo
+// TODO 'route model binding' para las rutas de obra
+// TODO rutas agrupadas por bloques
+// TODO rediseño rutas ej. una ruta para poner crítica otra para modificarla
+// TODO chequear consultas & inyección de dependencias (service container)
+// TODO sección de la web: listas propias/usuarios
+// TODO honeypot (tallpad)
+// TODO evaluación/criticas en el perfil de usuario
+// TODO podcast en el permanent layout
+// TODO avatar de usuario, completar las posibilidades de edición de datos personales en pagina de usuario
+// TODO sección fotos de pelicula
+// TODO roles para los gestores en una posible futura sección de gestion de datos
+// TODO notificaciones de algún tipo?? likes? criticas? estudiar (bootcamp ojear codigo)
+// TODO policies para las modificaciones (bootcamp ojear codigo)
+// TODO api de cines? hay algo?
+// TODO error 404 y otras paginas de error mejorar configuración
+// TODO mail, documentación al final de bootcamp
+// TODO despliegue? posibilidades
+// TODO ordenamiento: de críticas, otros ordenamientos para obras...
+// TODO buscador mejorar: paginación, mensajes con los resultados...
+// TODO session bbdd driver, tabla con almacenamiento de sesiones
+// TODO pop ups para el logueo, registro, likes...
+// TODO enlace correo filmxtra en footer layout lleva a formulario para contacto con nosotros: ssugerencias etc...
