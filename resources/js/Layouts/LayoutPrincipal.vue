def8b2a76305c3045748e97fe51b1990531eb179
victorFernandez173 <victor.fer.gor@proton.me>
2023-10-29 19:18:29 +0100

    ficheros variados añadidos

---
+++ resources/js/Layouts/LayoutPrincipal.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {Link} from "@inertiajs/vue3";
+import {onMounted} from "vue";
+import {initFlowbite} from "flowbite";
+import NavBar from "@/Components/NavBar.vue";
+import Footer from "@/Components/Footer.vue";
+
+// Para montar componentes Flowbite
+onMounted(() => {
+    initFlowbite();
+})
+</script>
+
+<template>
+    <!-- Barra de navegacion -->
+    <NavBar/>
+
+    <slot>
+        <!-- AQUÍ VA EL CONTENIDO QUE SE EL PASA AL LAYOUT    -->
+    </slot>
+
+    <!-- Pie de pagina -->
+    <Footer/>
+</template>
