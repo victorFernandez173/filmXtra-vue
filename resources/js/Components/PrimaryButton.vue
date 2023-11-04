08f3f56b2a13478415700ee904351a691252f95f
victorFernandez173 <victor.fer.gor@proton.me>
2023-11-04 11:20:40 +0100

    cambios para iniciar el desarrollo a partir de filmXtraVic 2

---
+++ resources/js/Components/PrimaryButton.vue
@@ -0,0 +1,7 @@
+<template>
+    <button
+        class="w-full m-auto text-white bg-flamingo hover:text-black focus:bg-flamingo focus:ring-flamingo focus:border-flamingo focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5"
+    >
+        <slot />
+    </button>
+</template>
