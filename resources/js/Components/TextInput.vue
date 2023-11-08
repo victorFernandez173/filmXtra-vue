4df14e3da8fad572df881e91a1f6725bb7c4e657
victorFernandez173 <victor.fer.gor@proton.me>
2023-11-08 19:54:05 +0100

    Estilos login y algunos componentes

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -23,7 +23,7 @@ defineExpose({ focus: () => input.value.focus() });
 
 <template>
     <input
-        class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+        class="border-[2px] border-gray-300 focus:ring-flamingo focus:ring-flamingo rounded-md shadow-sm focus:border-[2px] focus:border-flamingo"
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
         ref="input"
