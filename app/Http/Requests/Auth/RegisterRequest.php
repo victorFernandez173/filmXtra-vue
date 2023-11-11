b8d52d34164dd7ce2034c745b0616332a58f7845
victorFernandez173 <victor.fer.gor@proton.me>
2023-11-11 19:35:15 +0100

    Actualizacion dependencias y cambios menores

--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -24,8 +24,8 @@ class RegisterRequest extends FormRequest
     {
         return [
             'name' => ['required', 'max:25'],
-            // Ahora el bloque de email incorpora una regla que va a tener en cuenta que 'email' es único cuando 'social_type' es 'filmXtra'. Es decir, es posible que un usario se pueda registrar con dos cuentas: google y estándar. Así si el usuario ya tiene cuenta para un email con google o'auth y trata de registrarse de manera estándar con el formulario y ese mismo email, podrá: ya que el 'social_type' no va a coincidir. Pero si ya tiene cuenta estándar no podrá, porque el 'social_type' si va a coincidir: 'filmXtra'. El usuario podrá tener dos cuentas diferentes si se registra de las dos maneras o incluso más si se registra con diferentes proveedores o'auth: google y github.
-            'email' => ['required', 'max:60', 'email', Rule::unique('users', 'email')->where('social_type', 'filmXtra')],
+            // Ahora el bloque de email incorpora una regla que va a tener en cuenta que 'email' es único cuando 'social_type' es 'filmXtra'. Es decir, es posible que un usuario se pueda registrar con dos cuentas: google y estándar. Así si el usuario ya tiene cuenta para un email con google o'auth y trata de registrarse de manera estándar con el formulario y ese mismo email, podrá: ya que el 'social_type' no va a coincidir. Pero si ya tiene cuenta estándar no podrá, porque el 'social_type' si va a coincidir: 'filmXtra'. El usuario podrá tener dos cuentas diferentes si se registra de las dos maneras o incluso más si se registra con diferentes proveedores o'auth: google y github.
+            'email' => ['required', 'max:60', 'email:rfc,dns', Rule::unique('users', 'email')->where('social_type', 'filmXtra')],
             'password' => ['required', 'confirmed', Password::defaults()],
         ];
     }
@@ -43,10 +43,10 @@ class RegisterRequest extends FormRequest
             'email.required' => 'Introduzca email.',
             'email.email' => 'Formato de email no válido',
             'email.max' => 'Email: máximo 60 caracteres',
-            'email.unique' => 'Parece ser que ese email no está disponible...',
+            'email.unique' => 'Dirección email no disponible...',
             'password.required' => 'Introduzca contraseña',
             'password.confirmed' => 'Confirmación fallida',
-            'password.min' => 'Reglas: al menos 8 caracteres'
+            'password.min' => 'Al menos 8 caracteres'
         ];
     }
 }
