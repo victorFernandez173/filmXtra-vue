cd449d210a3bf5b0505552e7d694e8e604b63ef0
victorFernandez173 <victor.fer.gor@proton.me>
2023-11-12 13:42:17 +0100

    añadida paleta colores con uicolors en config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,21 @@ export default {
             },
             colors: {
                 'flamingo': '#e37f81',
+                /*Añadida con https://uicolors.app*/
+                'flamingo-paleta': {
+                    '50': '#fcf4f4',
+                    '100': '#fae6e6',
+                    '200': '#f6d2d3',
+                    '300': '#efb2b3',
+                    '400': '#e37f81',
+                    '500': '#d65d5f',
+                    '600': '#c14144',
+                    '700': '#a23335',
+                    '800': '#872d2f',
+                    '900': '#712b2c',
+                    '950': '#3c1314',
+                },
+
             },
             borderWidth: {
                 DEFAULT: '1px',
