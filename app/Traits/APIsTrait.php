320edc525ea90f80e5a0a1083263334feaf88bc3
victorFernandez173 <victor.fer.gor@proton.me>
2024-03-10 19:57:47 +0100

    controlada con try-catch en APIsTrait.php la obtención de la cita de la api, en caso de fallo, se devuelve cita de bbdd propia

--- app/Traits/APIsTrait.php
+++ app/Traits/APIsTrait.php
@@ -3,20 +3,30 @@
 namespace App\Traits;
 
 use Http;
+use Log;
+use Throwable;
 
 trait APIsTrait
 {
+    use CitasTrait;
+
     /**
      * Por medio de una consulta a la API Quotable
-     * Obtenemos una cita y la devovemos para usarla
+     *  Obtenemos una cita y la devovemos para usarla
      * @return string
      */
     public function citaQuotable(): string
     {
-        // Consultamos la api
-        $respuesta = json_decode(Http::get('https://api.quotable.io/quotes/random')->body(), true);
+        try {
+            // Consultamos la api
+            $respuesta = json_decode(Http::get('https://api.quotable.io/quotes/rasndom')->body(), true);
 
-        // Creamos y retornamos la cita
-        return '"' . $respuesta[0]['content'] . '" - ' . $respuesta[0]['author'];
+            // Creamos y retornamos la cita
+            return '"' . $respuesta[0]['content'] . '" - ' . $respuesta[0]['author'];
+        } catch(Throwable $e) {
+            Log::error($e->getMessage().': '.$e->getTraceAsString());
+
+            return $this->citaSobreCine();
+        }
     }
 }
