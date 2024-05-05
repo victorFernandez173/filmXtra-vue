1185259a49a9bfbfeda33d6ccf687c944f049584
Víctor Fernández <victor.fer.gor@proton.me>
2024-05-05 22:03:25 +0200

    TODO

--- routes/web.php
+++ routes/web.php
@@ -42,9 +42,7 @@ require __DIR__.'/auth.php';
 // TODO busqueda navbar reactivar
 // TODO aplicar o limpiar componentes no usados???
 // TODO redirección tras logueo: si duranto un logueo te vas a otra pagina como la de uno de los logueos de git o google y luego te logueas en el otro, al loguearse y redirigir no te devuelve a index si por ej partiste en un origen de index en filmXtra sino a la otra pagina de logueo de git o google que visitaste previamente, no se si podre reproducirlo,CONFIRMADO QUE OCURRE ESO
-
 // TODO REPRODUCIR funcionalidades filmXtraVic
-
 // TODO paginas de error todas, documentacion
 // TODO en la pagina de usuario se debería esconder el bloque de contraseña para cuentas o'auth
 // TODO al modificar la ruta de una obra, solo añadiendo caracteres en el trozo del titulo, se intenta acceder a esa obra pero claro, NO EXISTE y da un error de array[0] nulo porque claro no hay obra ESTUDIAR
