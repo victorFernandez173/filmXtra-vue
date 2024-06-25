54f735e8c55e831b252dee348b71548736b61d2d
Víctor Fernández <victor.fer.gor@proton.me>
2024-06-25 16:44:40 +0200

    TODOs...

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,10 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-// TODO revisar componentes Uno a uno, REVISADOS: AppLogoIndex,
+// TODO revisar componentes Uno a uno, REVISADOS: AppLogoIndex, Carrusel, Checkbox
+
+
+
 // TODO usar Navlink/RESPONSIVENAVLINK en Navbar y otros components if possible
 // TODO revisar Pages y Layouts, REVISADOS:
 // TODO revisar paso por paso funcionamiento de los CONTROLLERS y REQUESTS, REVISADOS:
