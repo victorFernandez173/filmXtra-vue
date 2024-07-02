384a1672f45391e247dd8001a23aec0b05e5ed6f
Víctor Fernández <victor.fer.gor@proton.me>
2024-07-02 17:58:50 +0200

    TODOs

--- routes/web.php
+++ routes/web.php
@@ -27,19 +27,19 @@ require __DIR__.'/auth.php';
 
 // TODO revisar paso por paso funcionamiento de los CONTROLLERS y REQUESTS, REVISADOS:
 // TODO revisar resto ficheros, REVISADOS:
-// TODO lang english?? como aplicar mediante funcionalidad lang
-// TODO modo oscuro
 // TODO index, posters aleatorios? siempre alien al principio???
 // TODO implementar columnas tablas personalizadas para login y register, tb email personalizado? etc
+// TODO en la pagina de usuario se debería esconder el bloque de contraseña para cuentas o'auth
+
+
+// TODO lang english?? como aplicar mediante funcionalidad lang
+// TODO modo oscuro
 // TODO logo navbar +pequeño tam. movil, incluso icon,
 // TODO validaciones notacion OBJETOS en lugar de arrays
 // TODO citasYapiTraits unir en una clase e instanciar en el service container para testear el IoC????o mejorar de alguna manera
-// TODO evento VERIFIED
 // TODO compactar citas en INDEX??? Mejorar estilos recuadro blanco
-// TODO busqueda navbar reactivar
 // TODO redirección tras logueo: si duranto un logueo te vas a otra pagina como la de uno de los logueos de git o google y luego te logueas en el otro, al loguearse y redirigir no te devuelve a index si por ej partiste en un origen de index en filmXtra sino a la otra pagina de logueo de git o google que visitaste previamente, no se si podre reproducirlo,CONFIRMADO QUE OCURRE ESO
 // TODO REPRODUCIR funcionalidades filmXtraVic
-// TODO en la pagina de usuario se debería esconder el bloque de contraseña para cuentas o'auth
 // TODO al modificar la ruta de una obra, solo añadiendo caracteres en el trozo del titulo, se intenta acceder a esa obra pero claro, NO EXISTE y da un error de array[0] nulo porque claro no hay obra ESTUDIAR
 // TODO eventserviceprovider event envio mail registros puede ser util?
 // TODO event en RegisteredUserController, implementar para algo, event REGISTERED, VERIFIED...
@@ -54,6 +54,6 @@ require __DIR__.'/auth.php';
 // TODO roles para los gestores en una posible futura sección de gestion de datos
 // TODO notificaciones de algún tipo?? likes? criticas? estudiar (bootcamp ojear codigo)
 // TODO policies para las modificaciones (bootcamp ojear codigo)
-// TODO buscador mejorar: paginación, mensajes con los resultados...
+// TODO busqueda navbar reactivar;;;buscador mejorar: paginación, mensajes con los resultados...
 // TODO session bbdd driver, tabla con almacenamiento de sesiones
 // TODO enlace correo filmxtra en footer layout lleva a formulario para contacto con nosotros: ssugerencias etc...
