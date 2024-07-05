8c10f739893cdb1e91069d7b3810ff087574ed2a
Víctor Fernández <victor.fer.gor@proton.me>
2024-07-05 14:35:17 +0200

    TODOs

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,6 @@ require __DIR__.'/auth.php';
 
 // TODO españolizar columnas/datos de user como created_at etc....
 // TODO logo navbar +pequeño tam. movil, incluso icon,
-// TODO compactar citas en INDEX??? Mejorar estilos recuadro blanco
 // TODO redirección tras logueo: si duranto un logueo te vas a otra pagina como la de uno de los logueos de git o google y luego te logueas en el otro, al loguearse y redirigir no te devuelve a index si por ej partiste en un origen de index en filmXtra sino a la otra pagina de logueo de git o google que visitaste previamente, no se si podre reproducirlo,CONFIRMADO QUE OCURRE ESO
 // TODO eventserviceprovider event envio mail registros puede ser util?
 // TODO event en RegisteredUserController, implementar para algo, event REGISTERED, VERIFIED...
