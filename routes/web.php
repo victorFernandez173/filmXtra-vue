2afa24e9e2c3a2124ba924c509c3ea85c1634c4b
Víctor Fernández <victor.fer.gor@proton.me>
2024-07-05 12:55:01 +0200

    TODOs

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,8 @@ require __DIR__.'/auth.php';
 // TODO logo navbar +pequeño tam. movil, incluso icon,
 // TODO compactar citas en INDEX??? Mejorar estilos recuadro blanco
 // TODO redirección tras logueo: si duranto un logueo te vas a otra pagina como la de uno de los logueos de git o google y luego te logueas en el otro, al loguearse y redirigir no te devuelve a index si por ej partiste en un origen de index en filmXtra sino a la otra pagina de logueo de git o google que visitaste previamente, no se si podre reproducirlo,CONFIRMADO QUE OCURRE ESO
+// TODO eventserviceprovider event envio mail registros puede ser util?
+// TODO event en RegisteredUserController, implementar para algo, event REGISTERED, VERIFIED...
 
 
 // TODO lang english?? como aplicar mediante funcionalidad lang
@@ -38,8 +40,6 @@ require __DIR__.'/auth.php';
 // TODO citasYapiTraits unir en una clase e instanciar en el service container para testear el IoC????o mejorar de alguna manera
 // TODO REPRODUCIR funcionalidades filmXtraVic
 // TODO al modificar la ruta de una obra, solo añadiendo caracteres en el trozo del titulo, se intenta acceder a esa obra pero claro, NO EXISTE y da un error de array[0] nulo porque claro no hay obra ESTUDIAR
-// TODO eventserviceprovider event envio mail registros puede ser util?
-// TODO event en RegisteredUserController, implementar para algo, event REGISTERED, VERIFIED...
 // TODO 'route model binding' para las rutas de obra
 // TODO rutas agrupadas por bloques
 // TODO sección de la web: listas propias/usuarios
