2ef0701d29f6c3e75ea9c4f6f53a50c8c672c8d2
Víctor Fernández <victor.fer.gor@proton.me>
2024-07-05 16:41:30 +0200

    TODOs

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 
-// TODO tablas failed_jobs, password_reset_tokens y personal_access_tokens WHAT FOR????
+// TODO tablas failed_jobs, password_reset_tokens y personal_access_tokens WHAT FOR???? DELETE????
 // TODO logo navbar +pequeño tam. movil, incluso icon,
 // TODO redirección tras logueo: si duranto un logueo te vas a otra pagina como la de uno de los logueos de git o google y luego te logueas en el otro, al loguearse y redirigir no te devuelve a index si por ej partiste en un origen de index en filmXtra sino a la otra pagina de logueo de git o google que visitaste previamente, no se si podre reproducirlo,CONFIRMADO QUE OCURRE ESO
 // TODO eventserviceprovider event envio mail registros puede ser util?
