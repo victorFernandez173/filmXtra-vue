9c17fcfa4ed62c9495cbf1c41a6c04e2dffaab21
Víctor Fernández <victor.fer.gor@proton.me>
2024-07-05 12:33:49 +0200

    TODOs

--- routes/web.php
+++ routes/web.php
@@ -25,18 +25,17 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 
-// TODO revisar paso por paso funcionamiento de los CONTROLLERS y REQUESTS, REVISADOS:
 // TODO implementar columnas tablas personalizadas para login y register, tb email personalizado? etc
 // TODO españolizar columnas/datos de user como created_at etc....
+// TODO validaciones notacion OBJETOS en lugar de arrays
+// TODO logo navbar +pequeño tam. movil, incluso icon,
+// TODO compactar citas en INDEX??? Mejorar estilos recuadro blanco
+// TODO redirección tras logueo: si duranto un logueo te vas a otra pagina como la de uno de los logueos de git o google y luego te logueas en el otro, al loguearse y redirigir no te devuelve a index si por ej partiste en un origen de index en filmXtra sino a la otra pagina de logueo de git o google que visitaste previamente, no se si podre reproducirlo,CONFIRMADO QUE OCURRE ESO
 
 
 // TODO lang english?? como aplicar mediante funcionalidad lang
 // TODO modo oscuro
-// TODO logo navbar +pequeño tam. movil, incluso icon,
-// TODO validaciones notacion OBJETOS en lugar de arrays
 // TODO citasYapiTraits unir en una clase e instanciar en el service container para testear el IoC????o mejorar de alguna manera
-// TODO compactar citas en INDEX??? Mejorar estilos recuadro blanco
-// TODO redirección tras logueo: si duranto un logueo te vas a otra pagina como la de uno de los logueos de git o google y luego te logueas en el otro, al loguearse y redirigir no te devuelve a index si por ej partiste en un origen de index en filmXtra sino a la otra pagina de logueo de git o google que visitaste previamente, no se si podre reproducirlo,CONFIRMADO QUE OCURRE ESO
 // TODO REPRODUCIR funcionalidades filmXtraVic
 // TODO al modificar la ruta de una obra, solo añadiendo caracteres en el trozo del titulo, se intenta acceder a esa obra pero claro, NO EXISTE y da un error de array[0] nulo porque claro no hay obra ESTUDIAR
 // TODO eventserviceprovider event envio mail registros puede ser util?
