b4bc8d26c63354f75e26d3376e408e5b18223207
Víctor Fernández <victor.fer.gor@proton.me>
2024-07-11 16:07:04 +0200

    pequeñas correciones en texto email verificación exitosa

--- resources/views/emails/bienvenida.blade.php
+++ resources/views/emails/bienvenida.blade.php
@@ -5,10 +5,10 @@
     <h4>Hola {{$usuario->usuario}}:</h4>
     <br>
     <p class="azul-oscuro" style="color: #2a4467; font-weight: 300;">
-        Gracias por validar tu direccion {{$usuario->email}} en filmXtra.
+        Gracias por validar tu email {{$usuario->email}} en filmXtra.
     </p>
     <br>
     <p class="azul-oscuro" style="color: #2a4467; font-weight: 300;">
-        Ahora ya podrás acceder a todas las funcionalidades de la web como poner crítica, dar likes a criticas y mucho mas...
+        Ahora ya podrás acceder a todas las funcionalidades de la web como poner críticas, dar likes a críticas y mucho más...
     </p>
 @endsection
