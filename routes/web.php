d91d20eca4c98744ac7453b0f9df16b74b4fe103
Víctor Fernández <victor.fer.gor@proton.me>
2024-07-11 17:57:14 +0200

    TODOs

--- routes/web.php
+++ routes/web.php
@@ -24,18 +24,19 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-// TODO register form, y si el user ya existe que pasa?????
 
-// TODO logo navbar +pequeño tam. movil, incluso icon,
 // TODO redirección tras logueo: si duranto un logueo te vas a otra pagina como la de uno de los logueos de git o google y luego te logueas en el otro, al loguearse y redirigir no te devuelve a index si por ej partiste en un origen de index en filmXtra sino a la otra pagina de logueo de git o google que visitaste previamente, no se si podre reproducirlo,CONFIRMADO QUE OCURRE ESO
-// TODO eventserviceprovider event envio mail registros puede ser util?
-// TODO lang english?? como aplicar mediante funcionalidad lang
-// TODO modo oscuro
 // TODO citasYapiTraits unir en una clase e instanciar en el service container para testear el IoC????o mejorar de alguna manera
-// TODO REPRODUCIR funcionalidades filmXtraVic
-// TODO al modificar la ruta de una obra, solo añadiendo caracteres en el trozo del titulo, se intenta acceder a esa obra pero claro, NO EXISTE y da un error de array[0] nulo porque claro no hay obra ESTUDIAR
 // TODO 'route model binding' para las rutas de obra
 // TODO rutas agrupadas por bloques
+// TODO busqueda navbar reactivar;;;buscador mejorar: paginación, mensajes con los resultados...
+
+
+// TODO logo navbar +pequeño tam. movil, incluso icon,
+// TODO lang english?? como aplicar mediante funcionalidad lang
+// TODO modo oscuro
+// TODO REPRODUCIR funcionalidades filmXtraVic
+// TODO al modificar la ruta de una obra, solo añadiendo caracteres en el trozo del titulo, se intenta acceder a esa obra pero claro, NO EXISTE y da un error de array[0] nulo porque claro no hay obra ESTUDIAR
 // TODO sección de la web: listas propias/usuarios
 // TODO honeypot (tallpad)
 // TODO evaluación/criticas en el perfil de usuario
@@ -45,6 +46,5 @@ require __DIR__.'/auth.php';
 // TODO roles para los gestores en una posible futura sección de gestion de datos
 // TODO notificaciones de algún tipo?? likes? criticas? estudiar (bootcamp ojear codigo)
 // TODO policies para las modificaciones (bootcamp ojear codigo)
-// TODO busqueda navbar reactivar;;;buscador mejorar: paginación, mensajes con los resultados...
 // TODO session bbdd driver, tabla con almacenamiento de sesiones
 // TODO enlace correo filmxtra en footer layout lleva a formulario para contacto con nosotros: ssugerencias etc...
