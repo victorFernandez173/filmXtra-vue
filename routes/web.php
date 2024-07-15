b3b4fb3598d0f77c173082aed35072563e6fc47a
Víctor Fernández <victor.fer.gor@proton.me>
2024-07-15 16:05:26 +0200

    TODOs

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,6 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 
-// TODO rutas agrupadas por bloques
 // TODO busqueda navbar reactivar;;;buscador mejorar: paginación, mensajes con los resultados...
 // TODO 'route model binding' para las rutas de obra
 // TODO REPRODUCIR funcionalidades filmXtraVic
