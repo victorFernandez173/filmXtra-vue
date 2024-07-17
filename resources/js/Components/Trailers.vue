f90b53bf7b8c9e4f4118e08f903889c3b424d464
Víctor Fernández <victor.fer.gor@proton.me>
2024-07-17 19:04:30 +0200

    pequeñas refactorizaciones en componente vue Trailers.vue

--- resources/js/Components/Trailers.vue
+++ resources/js/Components/Trailers.vue
@@ -4,14 +4,15 @@ defineProps(['obra']);
 
 <template>
         <!--Trailer-->
-        <div v-if="obra[0]['trailer']" class="mt-16 flex justify-center flex-wrap ">
+        <div v-if="obra.trailer" class="mt-16 flex justify-center flex-wrap ">
             <!-- Titulo de la seccion -->
-            <h3 class="w-full text-center text-flamingo font-bold underline text-2xl mb-4">Trailer de {{ obra[0]['titulo'] }}</h3>
+            <h3 class="w-full text-center text-flamingo font-bold underline text-2xl mb-4">Trailer de {{ obra.titulo }}</h3>
             <iframe
                 class="w-[100vw] h-auto aspect-video"
-                :src="obra[0]['trailer']['trailers']"
+                :src="obra.trailer.trailers"
                     title="YouTube video player"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowfullscreen></iframe>
+                    allowfullscreen>
+            </iframe>
         </div>
 </template>
