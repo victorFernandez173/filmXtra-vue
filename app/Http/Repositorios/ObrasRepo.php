11bcbeab4d8a8cd5f2b967ec70873fc65a78825d
Víctor Fernández <victor.fer.gor@proton.me>
2024-07-17 18:24:27 +0200

    refactorizaciones múltiples en el codigo de ObrasRepo.php para mejorar el codigo

--- app/Http/Repositorios/ObrasRepo.php
+++ app/Http/Repositorios/ObrasRepo.php
@@ -4,23 +4,28 @@ namespace App\Http\Repositorios;
 
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\PaginacionController;
+use App\Models\Like;
 use App\Models\Obra;
-use Exception;
+use App\Models\Secuela;
+use App\Models\Usuario;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\LengthAwarePaginator;
-use Illuminate\Support\Facades\DB;
+use LaravelIdea\Helper\App\Models\_IH_Obra_QB;
 
 class ObrasRepo extends Controller
 {
     /**
      * Para obtener los datos iniciales/base de la obra con relaciones
      * @param int $idObra
-     * @return Collection|Builder[]
+     * @return Obra|Builder|Model|_IH_Obra_QB
      */
-    static function obtenerDatosFichaObra(int $idObra): array|Collection
+    static function obtenerDatosFichaObra(int $idObra): Obra|Builder|Model|_IH_Obra_QB
     {
-        return Obra::with(['poster', 'secuela:obra_id,orden,saga', 'criticas', 'directors:nombre,edad,defuncion,pais', 'festivals:obra_id,nombre,edicion', 'profesionals:obra_id,medio_id,autor,contenido,fecha', 'evaluaciones:obra_id,usuario_id,evaluacion', 'actors:nombre,nombre_real,edad,defuncion,pais', 'generos:genero', 'trailer'])->where('id', $idObra)->get();
+        return Obra::with(['poster', 'secuela:obra_id,orden,saga', 'criticas', 'directors:nombre,edad,defuncion,pais', 'festivals:obra_id,nombre,edicion', 'profesionals.medio:id,nombre,web', 'evaluaciones:obra_id,usuario_id,evaluacion', 'actors:nombre,nombre_real,edad,defuncion,pais', 'generos:genero', 'trailer'])
+            ->where('id', $idObra)
+            ->first();
+
     }
 
     /**
@@ -32,7 +37,7 @@ class ObrasRepo extends Controller
     {
         $sumatorio = 0;
         foreach ($evaluaciones as $eva) {
-            $sumatorio += $eva['evaluacion'];
+            $sumatorio += $eva->evaluacion;
         }
         if (count($evaluaciones) == 0) {
             return 0;
@@ -50,58 +55,44 @@ class ObrasRepo extends Controller
         $criticasLikes = array();
         foreach ($criticas as $critica) {
             $criticasLikes[] = [
-                'id_critica' => $critica['id'],
-                'id_usuario' => $critica['usuario_id'],
-                'critica' => $critica['critica'],
-                'likes' => DB::table('likes')->where('critica_id', '=', $critica['id'])->count(),
-                'fecha' => $critica['modificada'],
-                'usuario' => DB::table('usuarios')->select('nombre', 'usuario')->where('id', '=', $critica['usuario_id'])->get(),
-                'gustadaPor' => DB::table('likes')->select('usuario_id')->where('critica_id', '=', $critica['id'])->get(),
+                'id_critica' => $critica->id,
+                'id_usuario' => $critica->usuario_id,
+                'critica'    => $critica->critica,
+                'likes'      => Like::where('critica_id', $critica->id)
+                    ->count(),
+                'fecha'      => $critica->modificada,
+                'usuario'    => Usuario::select(['usuario'])
+                    ->where('id', $critica->usuario_id)
+                    ->get(),
+                'gustadaPor' => Like::select('usuario_id')
+                    ->where('critica_id', $critica->id)
+                    ->get(),
             ];
         }
         return PaginacionController::paginar($criticasLikes, 4);
     }
 
     /**
-     * Para obtener secuelas/precuelas si las hubiera
+     * Para obtener: secuelas, precuelas o spinoffs si las hubiera
      * @param $obra
      * @return mixed
      */
     static function obtenerSecuelaPrecuela($obra): mixed
     {
-        // Si es una secuela esta obra
-        if (isset($obra[0]['secuela'])) {
+        // Si es parte de una saga esta obra
+        if ($obra->secuela) {
             // Obtenemos su orden en la saga
-            $orden = $obra[0]['secuela']['orden'];
-            $secuelaPrecuela = array();
-            // Obtenemos las array con las pelis de la saga
-            $secuelas = DB::table('secuelas')->select('saga', 'obra_id', 'orden')->where('saga', '=', $obra[0]['secuela']['saga'])->orderBy('orden', 'desc')->get();
-            //proceso el array $secuelas para orden + 1 (secuela) y orden - 1 (precuela) y orden = 0 (spin-off)
-            foreach ($secuelas as $esSecuela) {
-                if (($esSecuela->orden == 0 && $orden >= 1) || (($orden - 1) == $esSecuela->orden || ($orden + 1) == $esSecuela->orden)) {
-                    $secuelaPrecuela[] = $esSecuela->obra_id;
-                }
-            }
-            return Obra::with('poster', 'secuela:obra_id,orden')->find($secuelaPrecuela);
-        }
-        return null;
-    }
+            $orden = $obra->secuela->orden;
+            // Obtenemos las que haya: secuela, precuela y spin-offs según 'orden'
+            $secuelaPrecuela = Secuela::select('obra_id')
+                ->where('saga', $obra->secuela->saga)
+                ->whereIn('orden', [0, $orden + 1, $orden - 1])
+                ->orderBy('orden', 'desc')
+                ->get();
 
-    /**
-     * Para generar array con la info de las criticas profesionales
-     * @param $obra
-     * @return array|null
-     * @throws Exception
-     */
-    static function obtenerInfoMediosProfesionals($obra): ?array
-    {
-        if (count($obra[0]['profesionals']) > 0) {
-            $arrayProfesionals = array();
-            foreach ($obra[0]['profesionals'] as $profesional) {
-                $infoMedio = DB::table('medios')->find($profesional['medio_id']);
-                $arrayProfesionals[] = ['medio' => $infoMedio->nombre, 'web' => $infoMedio->web, 'contenido' => $profesional->contenido, 'autor' => $profesional['autor'], 'fecha' => $profesional['fecha']];
-            }
-            return $arrayProfesionals;
+            // Retornamos info de esas obras
+            return Obra::with('poster', 'secuela:obra_id,orden')
+                ->find($secuelaPrecuela);
         }
         return null;
     }
