549953ee2306054691f9a7c56354a107b9becb86
Víctor Fernández <victor.fer.gor@proton.me>
2024-07-25 14:17:03 +0200

    revertido Modal.vue original y creado nuevo ModalBusqueda.vue para la busqueda; eliminado el request BuscarTitulosRequest.php ya innecesario;

--- resources/js/Components/ModalBusqueda.vue
+++ resources/js/Components/ModalBusqueda.vue
@@ -62,7 +62,7 @@ const maxWidthClass = computed(() => {
 <template>
     <teleport to="body">
         <transition leave-active-class="duration-200">
-            <div v-show="show" class="fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50" scroll-region>
+            <div v-show="show" class="fixed inset-0 overflow-y-auto px-4 py-6 sm:px-0 z-50 my-[10vh]" scroll-region>
                 <transition
                     enter-active-class="ease-out duration-300"
                     enter-from-class="opacity-0"
@@ -72,7 +72,7 @@ const maxWidthClass = computed(() => {
                     leave-to-class="opacity-0"
                 >
                     <div v-show="show" class="fixed inset-0 transform transition-all" @click="close">
-                        <div class="absolute inset-0 bg-gray-500 opacity-75" />
+                        <div class="absolute inset-0" />
                     </div>
                 </transition>
 
@@ -86,7 +86,7 @@ const maxWidthClass = computed(() => {
                 >
                     <div
                         v-show="show"
-                        class="mb-6 bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full sm:mx-auto"
+                        class="mb-6 bg-white overflow-hidden shadow-2xl border-2 border-flamingo transform transition-all sm:w-full sm:mx-auto"
                         :class="maxWidthClass"
                     >
                         <slot v-if="show" />
