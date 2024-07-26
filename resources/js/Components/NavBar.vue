572f1e01c1ffc901f5ccc07124801aeb69e6d80c
Víctor Fernández <victor.fer.gor@proton.me>
2024-07-26 15:15:56 +0200

    mejoras/retoques en la responsividad de los elementos de la NavBar.vue

--- resources/js/Components/NavBar.vue
+++ resources/js/Components/NavBar.vue
@@ -79,8 +79,8 @@ const submit = () => {
         </div>
     </ModalBusqueda>
 
-    <nav class="bg-white border-gray-200  sticky top-0 z-50">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <nav class="bg-white border-gray-200 sticky top-0 z-50">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between lg:mx-auto p-4 relative">
             <!-- Logo de la pagina -->
             <Link :href="route('/')" class="flex items-center mb-2 lg:mb-0">
                 <img src="/images/logo.png" class="h-10 sm:h-14" alt="Logo FilmXtra" />
@@ -99,7 +99,7 @@ const submit = () => {
             </div>
             <!-- Apartado de usuario -->
             <div class="flex items-center font-bold lg:order-2">
-                <button type="button" class="flex text-sm bg-gray-800 rounded-full focus:ring-flamingo focus:ring-2 hover:ring-4 hover:ring-flamingo focus:flamingo" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                <button type="button" class="absolute right-16 lg:right-4 flex text-sm bg-gray-800 rounded-full focus:ring-flamingo focus:ring-2 hover:ring-4 hover:ring-flamingo focus:flamingo" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                     <span class="sr-only">Abrir menú de usuario</span>
                     <img class="w-8 h-8 rounded-full" src="/favicon.png" alt="Foto del usuario">
                 </button>
@@ -135,7 +135,7 @@ const submit = () => {
                 </div>
             </div>
             <!-- Links -->
-            <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="mobile-menu-2" aria-expanded="false">
+            <button data-collapse-toggle="mobile-menu-2" type="button" class="absolute right-3 inline-flex items-center p-2 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="mobile-menu-2" aria-expanded="false">
                 <span class="sr-only">Abrir menú principal</span>
                 <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
             </button>
