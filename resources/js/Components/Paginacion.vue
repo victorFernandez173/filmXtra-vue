381506eb73888126719d97f5cbe4b49ef274c6ab
Víctor Fernández <victor.fer.gor@proton.me>
2024-08-02 14:49:19 +0200

    estilos botonera componente Paginacion.vue

--- resources/js/Components/Paginacion.vue
+++ resources/js/Components/Paginacion.vue
@@ -8,10 +8,10 @@ defineProps(['obras']);
     <div class="mb-2" v-if="obras.links.length > 3">
         <div class="flex flex-wrap">
             <template v-for="(link) in obras.links">
-                <div v-if="link.url === null" class="mr-1 mb-1 px-4 py-3 leading-4 text-gray-400"
+                <div v-if="link.url === null" class="mr-1 mb-1 px-4 py-3 leading-4 text-gray-500"
                      v-html="link.label"/>
                 <Link v-else
-                      class="mr-1 mb-1 px-4 py-3 leading-4 hover:bg-white text-base hover:border hover:border-flamingo hover:border-solid border border-white border-solid"
+                      class="mr-1 mb-1 px-4 py-3 leading-4 hover:bg-white text-base "
                       :class="{ 'text-white bg-flamingo border-flamingo border-solid border-1 hover:text-black': link.active }" :href="link.url" v-html="link.label" preserve-state/>
             </template>
         </div>
