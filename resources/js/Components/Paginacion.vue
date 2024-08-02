6f4dc7f39ca05793488908de10fb4dc2e76b5cf5
Víctor Fernández <victor.fer.gor@proton.me>
2024-08-02 15:18:08 +0200

    desactivado funcionamiento boton activo que seguía recargando la página activa en cuestion

--- resources/js/Components/Paginacion.vue
+++ resources/js/Components/Paginacion.vue
@@ -8,11 +8,8 @@ defineProps(['obras']);
     <div class="mb-2" v-if="obras.links.length > 3">
         <div class="flex flex-wrap">
             <template v-for="(link) in obras.links">
-                <div v-if="link.url === null" class="mr-1 mb-1 px-4 py-3 leading-4 text-gray-500"
-                     v-html="link.label"/>
-                <Link v-else
-                      class="mr-1 mb-1 px-4 py-3 leading-4 hover:bg-white text-base "
-                      :class="{ 'text-white bg-flamingo border-flamingo border-solid border-1 hover:text-black': link.active }" :href="link.url" v-html="link.label" preserve-state/>
+                <div v-if="link.url === null" class="mr-1 mb-1 px-4 py-3 leading-4 text-gray-500" v-html="link.label"/>
+                <Link as="button" v-else class="mr-1 mb-1 px-4 py-3 leading-4 hover:bg-white text-base " :class="{ 'text-white bg-flamingo border-flamingo border-solid border-1 hover:text-black': link.active }" :href="link.url" v-html="link.label" :disabled="link.active" preserve-state/>
             </template>
         </div>
     </div>
