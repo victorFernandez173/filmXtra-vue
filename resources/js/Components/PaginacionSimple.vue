7f4c3195ab82caf26261fdfaf75b986c6cbccb5b
Víctor Fernández <victor.fer.gor@proton.me>
2024-08-05 13:28:52 +0200

    mejoras estilos y configuración componente PaginacionSimple.vue

--- resources/js/Components/PaginacionSimple.vue
+++ resources/js/Components/PaginacionSimple.vue
@@ -7,16 +7,19 @@ defineProps(['obras']);
 <template>
     <nav>
         <ul class="pagination">
-            <li v-if="obras.current_page === 1" disabled="true">Anterior dis
+            <li v-if="obras.current_page === 1" disabled="true">
+                « Anterior
             </li>
             <li v-else>
-                <Link :href="obras.prev_page_url" v-html="'Anterior en'" />
+                <Link as="button" class="mr-1 mb-1 px-4 py-3 leading-4 hover:bg-white text-base text-white bg-flamingo border-flamingo border-solid border-1 hover:text-black" :href="obras.prev_page_url" v-html="'« Anterior'" preserve-state/>
             </li>
 
             <li v-if="obras.next_page_url">
-                <Link :href="obras.next_page_url" v-html="'Siguiente en'" />
+                <Link as="button" class="mr-1 mb-1 px-4 py-3 leading-4 hover:bg-white text-base text-white bg-flamingo border-flamingo border-solid border-1 hover:text-black" :href="obras.next_page_url" v-html="'Siguiente »'" preserve-state/>
             </li>
-            <li v-else disabled="true">Siguiente dis</li>
+            <li v-else disabled="true">
+                Siguiente »
+            </li>
         </ul>
     </nav>
 </template>
