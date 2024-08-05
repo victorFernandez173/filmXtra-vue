9137c470125779e7a35fc21dab2f1b71c26a7b7a
Víctor Fernández <victor.fer.gor@proton.me>
2024-08-05 11:27:44 +0200

    creado componente para paginación simple (sin estilos, funcional)

---
+++ resources/js/Components/PaginacionSimple.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {Link} from "@inertiajs/vue3";
+
+defineProps(['obras']);
+</script>
+
+<template>
+    <nav>
+        <ul class="pagination">
+            <li v-if="obras.current_page === 1" disabled="true">Anterior dis
+            </li>
+            <li v-else>
+                <Link :href="obras.prev_page_url" v-html="'Anterior en'" />
+            </li>
+
+            <li v-if="obras.next_page_url">
+                <Link :href="obras.next_page_url" v-html="'Siguiente en'" />
+            </li>
+            <li v-else disabled="true">Siguiente dis</li>
+        </ul>
+    </nav>
+</template>
