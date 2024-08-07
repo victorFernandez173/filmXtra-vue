1440dca4969ad0f2c50e25c6438a859412e7fb19
Víctor Fernández <victor.fer.gor@proton.me>
2024-08-07 11:34:58 +0200

    funcion criticar y pasando prop necesario para control de la critica del usuario

--- app/Http/Controllers/CriticasController.php
+++ app/Http/Controllers/CriticasController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Repositorios\CriticasRepo;
 use App\Http\Repositorios\ObrasRepo;
+use App\Http\Requests\CriticarObraRequest;
 use App\Http\Requests\EvaluarObraRequest;
+use App\Models\Critica;
 use App\Models\Evaluacion;
 use App\Models\Like;
 use Auth;
@@ -21,20 +23,8 @@ class CriticasController extends Controller
             [
                 'obra'              => $obra,
                 'mediaEvaluaciones' => ObrasRepo::obtenerObraNotaMedia($tituloSlug),
-                // Criticas relacionadas con esta película
-                //                'peliculaCriticas'     =>
-                //                    Critica::select(
-                //                        [
-                //                            'usuario_id',
-                //                            'obra_id',
-                //                            'critica'
-                //                        ]
-                //                    )->where(
-                //                        'obra_id',
-                //                        $obra->id
-                //                    )->get(),
-                'evaluacionUsuario' => Evaluacion::where('usuario_id', Auth::user()->id)->where('obra_id', $obra->id)
-                    ->pluck('evaluacion'),
+                'criticaUsuario'    => Critica::where('usuario_id', Auth::id())->where('obra_id', $obra->id)->pluck('critica'),
+                'evaluacionUsuario' => Evaluacion::where('usuario_id', Auth::id())->where('obra_id', $obra->id)->pluck('evaluacion'),
                 'criticas'          => CriticasRepo::obtenerArrayInfoCriticas($obra->criticas)->paginate(3),
                 'nGifs'             => count(glob(public_path('/gif/') . '*'))
             ]
@@ -75,6 +65,24 @@ class CriticasController extends Controller
             ],
             [
                 'evaluacion' => $request->evaluacion,
+            ]
+        );
+    }
+
+    /**
+     * Para poner o modificar critica de una obra
+     * @param CriticarObraRequest $request
+     * @return void
+     */
+    public function criticar(CriticarObraRequest $request)
+    {
+        Critica::updateOrCreate(
+            [
+                'usuario_id' => Auth::id(),
+                'obra_id'    => $request->obra_id
+            ],
+            [
+                'critica'    => $request->critica,
             ]
         );
     }
