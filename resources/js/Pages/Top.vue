f739a1df972edacc05106d4f608a6c9ef21f13ca
Víctor Fernández <victor.fer.gor@proton.me>
2024-08-09 13:41:25 +0200

    cambios en la la vista top para aplicar la paginación por scroll con vue useIntersectionObserver

--- resources/js/Pages/Top.vue
+++ resources/js/Pages/Top.vue
@@ -9,13 +9,43 @@ export default {
 
 <!--script estandar para lo habitual-->
 <script setup>
-import {Head} from "@inertiajs/vue3";
+import { Head } from "@inertiajs/vue3";
 import BarraLateral from "../Components/BarraLateral.vue";
 import FormularioFiltrado from "../Components/FormularioFiltrado.vue";
-import Paginacion from "../Components/Paginacion.vue";
 import FichaPeli from "@/Components/FichaPeli.vue";
+import { useIntersectionObserver } from '@vueuse/core';
+import { ref } from "vue";
+import axios from "axios";
 
-defineProps(['obras', 'titulo', 'filtros', 'pionera']);
+const props = defineProps(['obras', 'paises', 'generos', 'filtros', 'pionera']);
+
+const gatilloScroll = ref(null);
+
+// Para controllar con vue useIntersectionObserver el scrolling infinito
+const { stop } = useIntersectionObserver(
+    // Elemento observado para actuar
+    gatilloScroll,
+    ([{ isIntersecting }]) => {
+        // Si llegamos al elemento
+        if (isIntersecting) {
+            // Clonamos el objeto para que pueda ser reutilizado sin errores
+            const obrasClonadas = JSON.parse(JSON.stringify(props.obras));
+            // Axios con url basica, parametros y cursor para especificar datos a pasar
+            axios.get(`${props.obras.path}?cursor=${props.obras.next_cursor}${props.filtros.pais?'&pais='+props.filtros.pais:''}${props.filtros.genero?'&genero='+props.filtros.genero:''}${props.filtros.desde?'&desde='+props.filtros.desde:''}${props.filtros.hasta?'&hasta='+props.filtros.hasta:''}`).then((response) => {
+                // En caso de que haya que paginar
+                if(response.data.tienePaginas) {
+                    props.obras.data = [...obrasClonadas.data, ...response.data.obras.data];
+                    props.obras.prev_cursor = response.data.obras.prev_cursor;
+                    props.obras.next_cursor = response.data.obras.next_cursor;
+                }
+                // Si response no tiene next_cursor, no hay más datos, se para el scrolling
+                if (!response.data.obras.next_cursor) {
+                    stop();
+                }
+            })
+        }
+    }
+);
 </script>
 
 <template>
@@ -31,9 +61,11 @@ defineProps(['obras', 'titulo', 'filtros', 'pionera']);
         <!-- Contenido -->
         <div class="lg:w-[87vw] xl:w-[89vw] 2xl:w-[92vw] mx-auto">
             <!-- Título -->
-            <h1 class="w-full mt-2 font-oswald text-center text-5xl h-[8vh] text-flamingo">Top FilmXtra</h1>
+            <h1 class="w-full mt-2 font-oswald text-center text-5xl h-[8vh] text-flamingo">
+                Top FilmXtra
+            </h1>
             <!-- Formulario de filtrado -->
-            <FormularioFiltrado :paises="$page.props.paises" :generos="$page.props.generos" :pionera="$page.props.pionera"  />
+            <FormularioFiltrado :paises="paises" :generos="generos" :pionera="pionera" :filtros="filtros"  />
             <!-- Mensaje de filtrado condicionado en funcion de parámetros del formulario -->
             <p v-if="filtros.genero || filtros.pais || filtros.desde || filtros.hasta" class="w-full text-center mt-1">
                 Filtros&rarr; &nbsp;
@@ -55,10 +87,8 @@ defineProps(['obras', 'titulo', 'filtros', 'pionera']);
             <div class="container grid grid-cols-1 m-auto my-8">
                 <FichaPeli v-for="obra in obras.data" :obra="obra" :info="true" />
             </div>
-            <!-- Componente para la paginación -->
-            <div class="w-full flex mt-8 mb-2">
-                <Paginacion class="mx-auto" :datos="obras"/>
-            </div>
+            <!-- Para el infinite scrolling -->
+            <div ref="gatilloScroll"></div>
         </div>
     </div>
 </template>
