4437fdf5157cc1eb2e58892cb8432063b62b8f15
Víctor Fernández <victor.fer.gor@proton.me>
2024-08-11 13:09:45 +0200

    estilos: ajustada anchura bloques forms en perfil cuenta

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -30,13 +30,13 @@ defineProps({
     </Head>
 
     <auth-layout>
-        <div class="py-32 flex flex-col items-center h-fit">
+        <div class="py-16 md:py-32 flex flex-col items-center h-fit">
 
             <div class="mb-16 p-4 max-w-fit bg-white">
                 <h2 class="font-semibold text-xl text-gray-800 leading-tight">Perfil</h2>
             </div>
 
-            <div :class="$page.props.auth.user.login_tipo_id === 1 && $page.props.auth.user.email_verificado_fecha ? 'lg:grid-cols-2' : 'lg:grid-cols-1'" class="w-full lg:w-fit grid grid-cols-1 mb-16 p-4 sm:p-8 bg-white text-center justify-items-center">
+            <div :class="$page.props.auth.user.login_tipo_id === 1 && $page.props.auth.user.email_verificado_fecha ? 'lg:grid-cols-2' : 'lg:grid-cols-1'" class="w-[95%] lg:w-fit grid grid-cols-1 mb-16 p-4 sm:p-8 bg-white text-center justify-items-center">
                 <update-profile-information-form
                     :must-verify-email="mustVerifyEmail"
                     :status="status"
@@ -46,7 +46,7 @@ defineProps({
                 <update-password-form v-if="$page.props.auth.user.login_tipo_id === 1 && $page.props.auth.user.email_verificado_fecha" class="m-4 mt-24 lg:mt-4 mx-16 w-4/5  sm:w-3/5 lg:w-4/5" />
             </div>
 
-            <div class="w-full lg:w-fit mb-12 p-4 sm:p-8 bg-white shadow text-center">
+            <div class="w-[95%] lg:w-fit mb-12 p-4 sm:p-8 bg-white shadow text-center">
                     <delete-user-form class="m-4" />
             </div>
         </div>
