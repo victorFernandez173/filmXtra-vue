56c37fe6d7f5e5843f46fdc65df46e96f4aca11c
Víctor Fernández <victor.fer.gor@proton.me>
2024-08-11 12:28:42 +0200

    Eliminadas variables de usuario en script y pasados props directamente al template de Edit.vue

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -10,7 +10,7 @@ import AuthLayout from "@/Layouts/AuthLayout.vue";
 import DeleteUserForm from './Partials/DeleteUserForm.vue';
 import UpdatePasswordForm from './Partials/UpdatePasswordForm.vue';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm.vue';
-import {Head, usePage} from '@inertiajs/vue3';
+import { Head } from '@inertiajs/vue3';
 
 defineProps({
     mustVerifyEmail: {
@@ -20,9 +20,6 @@ defineProps({
         type: String,
     },
 });
-
-const userLoginTipo = usePage().props.auth.user.login_tipo_id;
-const verificado = usePage().props.auth.user.email_verificado_fecha;
 </script>
 
 <template>
@@ -39,14 +36,14 @@ const verificado = usePage().props.auth.user.email_verificado_fecha;
                 <h2 class="font-semibold text-xl text-gray-800 leading-tight">Perfil</h2>
             </div>
 
-            <div :class="userLoginTipo === 1 && verificado ? 'lg:grid-cols-2' : 'lg:grid-cols-1'" class="w-full lg:w-fit grid grid-cols-1 mb-16 p-4 sm:p-8 bg-white text-center justify-items-center">
+            <div :class="$page.props.auth.user.login_tipo_id === 1 && $page.props.auth.user.email_verificado_fecha ? 'lg:grid-cols-2' : 'lg:grid-cols-1'" class="w-full lg:w-fit grid grid-cols-1 mb-16 p-4 sm:p-8 bg-white text-center justify-items-center">
                 <update-profile-information-form
                     :must-verify-email="mustVerifyEmail"
                     :status="status"
                     class="my-4 mx-16 w-4/5 sm:w-3/5 lg:w-4/5"
                 />
 
-                <update-password-form v-if="userLoginTipo === 1 && verificado" class="m-4 mt-24 lg:mt-4 mx-16 w-4/5  sm:w-3/5 lg:w-4/5" />
+                <update-password-form v-if="$page.props.auth.user.login_tipo_id === 1 && $page.props.auth.user.email_verificado_fecha" class="m-4 mt-24 lg:mt-4 mx-16 w-4/5  sm:w-3/5 lg:w-4/5" />
             </div>
 
             <div class="w-full lg:w-fit mb-12 p-4 sm:p-8 bg-white shadow text-center">
