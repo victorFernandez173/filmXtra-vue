55875f4ab79e5219f6b049de23f316d81b8e9fd1
Víctor Fernández <victor.fer.gor@proton.me>
2024-08-12 11:30:52 +0200

    TODOs y pequeños ajustes margen barra-lateral

--- resources/js/Components/BarraLateral.vue
+++ resources/js/Components/BarraLateral.vue
@@ -11,7 +11,7 @@ const props = defineProps({
 <template>
     <section class="h-full bg-flamingo p-4 pb-8 hover:[&>ul>li]:text-white [&>ul>li]:truncate">
         <!-- Genero -->
-        <h5 class="pt-3 font-bold underline">Género</h5>
+        <h5 class="mt-3 font-bold underline">Género</h5>
         <ul>
             <li v-for="genero in generos">
                 <Link :href="route('top')" :data="{ genero: genero }">
@@ -21,7 +21,7 @@ const props = defineProps({
         </ul>
 
         <!-- Decada -->
-        <h5 class="pt-3 font-bold underline">Década</h5>
+        <h5 class="mt-5 font-bold underline">Década</h5>
         <ul>
             <li v-for="decada in decadas">
                 <Link :href="route('top')" :data="{ desde : decada.toString(), hasta : (decada+9) }">
@@ -31,7 +31,7 @@ const props = defineProps({
         </ul>
 
         <!-- Pais -->
-        <h5 class="pt-3 font-bold underline">País</h5>
+        <h5 class="mt-6 font-bold underline">País</h5>
         <ul>
             <li v-for="pais in paises">
                 <Link :href="route('top')" :data="{ pais : pais }">
