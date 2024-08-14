cc53fcc1f2192170685d88a2193be65f1068b65f
Víctor Fernández <victor.fer.gor@proton.me>
2024-08-14 11:36:00 +0200

    correcciones estilos en ErrorLayout.vue y pagina de error 404 para la responsividad

--- resources/js/Layouts/ErrorLayout.vue
+++ resources/js/Layouts/ErrorLayout.vue
@@ -1,6 +1,6 @@
 <template>
-    <section class="grid h-screen place-items-center bg-white px-6 sm:py-32 lg:px-8 bg-cover bg-[url('/error/error-fondo.gif')]">
-        <div class="flex flex-col justify-center items-center bg-neutral-50/75 p-20 h-[80vh] w-[50vw]">
+    <section class="grid h-screen place-items-center bg-white bg-cover bg-[url('/error/error-fondo.gif')]">
+        <div class="flex flex-col justify-center items-center bg-neutral-50/75 p-5 w-[95vw] md:w-[75vw] xl:w-[50vw]">
             <slot />
         </div>
     </section>
--- resources/js/Pages/Errors/404.vue
+++ resources/js/Pages/Errors/404.vue
@@ -11,16 +11,16 @@ import { Head, Link } from "@inertiajs/vue3";
         <meta name="description" content="Página de error">
     </Head>
     <error-layout>
-        <img src="/images/logo-error.png" class="m-auto w-60 mb-10" alt="Logo Error 404">
-        <p class="mt-5 mb-12 text-2xl leading-7 text-gray-800">
+        <img src="/images/logo-error.png" class="m-auto w-[150px] md:w-[300px] my-10" alt="Logo Error 404">
+        <p class="mb-10 text-lg lg:text-2xl leading-7 text-gray-800 text-center">
             La pagina que buscas no existe, lo sentimos.
         </p>
-        <img src="/error/error-gif.gif" class="m-auto mt-7 w-80" alt="Gif error cine">
-        <div class="mt-10 flex items-center justify-center gap-x-6">
+        <img src="/error/error-gif.gif" class="mx-auto mb-10 w-80" alt="Gif error cine">
+        <div class="flex items-center justify-center">
             <Link :href="route('/')" class="hover:text-flamingo hover:bg-black bg-flamingo px-3.5 py-2.5 text-sm font-semibold text-white">
                 Ir a inicio &rarr;
             </Link>
         </div>
-        <img src="/images/logo.png" class="m-auto w-60 mt-10" alt="Logo FilmXtra">
+        <img src="/images/logo.png" class="m-auto lg:w-60 my-10" alt="Logo FilmXtra">
     </error-layout>
 </template>
