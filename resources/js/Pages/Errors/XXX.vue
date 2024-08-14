31bdfd241e4ae67bbbd42f5d72081b10ce795613
Víctor Fernández <victor.fer.gor@proton.me>
2024-08-14 11:42:03 +0200

    correcciones estilos en pagina de error global para la responsividad

--- resources/js/Pages/Errors/XXX.vue
+++ resources/js/Pages/Errors/XXX.vue
@@ -9,7 +9,7 @@ const props = defineProps({
 
 const message = computed(() => {
     return props.status >= 500 ?
-        'Parece que estamos teniendo problemas.' : 'Parece haber algún error con tu navegador.';
+        'Parece que estamos teniendo problemas.' : 'Parece que ha habido algún error.';
 });
 </script>
 
@@ -21,18 +21,18 @@ const message = computed(() => {
         <meta name="description" content="Página de error">
     </Head>
     <error-layout>
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+        <h1 class="text-4xl font-bold tracking-tight text-gray-900 md:text-5xl my-10">
             ERROR {{ status }}
         </h1>
-        <p class="my-10 text-2xl leading-7 text-gray-800">
+        <p class="mb-10 text-lg md:text-2xl leading-7 text-gray-800 text-center">
             {{ message }}
         </p>
-        <img src="/error/error-gif.gif" class="m-auto mt-7 w-80" alt="Gif error cine">
-        <div class="flex items-center justify-center gap-x-6">
+        <img src="/error/error-gif.gif" class="m-auto mb-10 w-80" alt="Gif error cine">
+        <div class="flex items-center justify-center">
             <Link :href="route('/')" class="hover:text-flamingo hover:bg-black bg-flamingo px-3.5 py-2.5 text-sm font-semibold text-white">
                 Ir a inicio &rarr;
             </Link>
         </div>
-        <img src="/images/logo.png" class="w-60 mt-10" alt="Logo FilmXtra">
+        <img src="/images/logo.png" class="w-60 my-10" alt="Logo FilmXtra">
     </error-layout>
 </template>
