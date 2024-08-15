c3355c265f36b1b8703167f847b700f608561d3f
Víctor Fernández <victor.fer.gor@proton.me>
2024-08-15 11:38:55 +0200

    test5

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -37,13 +37,13 @@ defineProps({
             </div>
 
             <div :class="$page.props.auth.user.login_tipo_id === 1 && $page.props.auth.user.email_verificado_fecha ? 'lg:grid-cols-2' : 'lg:grid-cols-1'" class="w-[95%] lg:w-fit grid grid-cols-1 mb-16 p-4 sm:p-8 bg-white text-center justify-items-center">
-                <update-profile-information-form
-                    :must-verify-email="mustVerifyEmail"
-                    :status="status"
-                    class="my-4 mx-16 w-4/5 sm:w-3/5 lg:w-4/5"
-                />
+<!--                <update-profile-information-form-->
+<!--                    :must-verify-email="mustVerifyEmail"-->
+<!--                    :status="status"-->
+<!--                    class="my-4 mx-16 w-4/5 sm:w-3/5 lg:w-4/5"-->
+<!--                />-->
 
-                <update-password-form v-if="$page.props.auth.user.login_tipo_id === 1 && $page.props.auth.user.email_verificado_fecha" class="m-4 mt-24 lg:mt-4 mx-16 w-4/5  sm:w-3/5 lg:w-4/5" />
+<!--                <update-password-form v-if="$page.props.auth.user.login_tipo_id === 1 && $page.props.auth.user.email_verificado_fecha" class="m-4 mt-24 lg:mt-4 mx-16 w-4/5  sm:w-3/5 lg:w-4/5" />-->
             </div>
 
             <div class="w-[95%] lg:w-fit mb-12 p-4 sm:p-8 bg-white shadow text-center">
