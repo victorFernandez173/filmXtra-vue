6e63d74bfbc27eebf83b736809d1c2cbc93bac43
Víctor Fernández <victor.fer.gor@proton.me>
2024-08-23 10:08:02 +0200

    probando nuevos estilos en PrimaryButton.vue

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="m-auto text-black bg-flamingo hover:text-flamingo hover:bg-black active:bg-flamingo active:ring-black active:border-black active:border-[2px] border-flamingo border-[2px] focus:outline-none font-medium text-sm px-5 py-2.5">
+    <button class="m-auto text-black bg-flamingo hover:text-flamingo hover:bg-black active:bg-flamingo  active:border-black focus:outline-none focus:border-black active:text-black active:border-[2px] border-flamingo border-[2px] font-medium text-sm px-5 py-2.5">
         <slot />
     </button>
 </template>
