86b29678b2024fa632fbb0e012608f0ca58d5cbb
Víctor Fernández <victor.fer.gor@proton.me>
2024-09-13 22:52:03 +0200

    mas vistas/componentes adaptadas a las traducciones (componentes finalizados)

--- resources/js/Components/SelectRango.vue
+++ resources/js/Components/SelectRango.vue
@@ -15,7 +15,7 @@ emit('emision', valorSelect);
 <template>
     <div class="m-auto w-11/12 py-[10px] px-0">
         <select v-model="valorSelect" class="bg-gray-50 border border-gray-300 text-gray-900 text-base focus:ring-red-400 focus:border-red-400 block w-full p-2.5">
-            <option disabled>{{valor}}</option>
+            <option disabled>{{ valor }}</option>
             <option v-for="n in limite">{{ limite - n }}</option>
         </select>
     </div>
--- resources/js/Components/Trailers.vue
+++ resources/js/Components/Trailers.vue
@@ -8,7 +8,7 @@ const props = defineProps({
         <!--Trailer-->
         <div v-if="obra.trailer" class="mt-16 flex justify-center flex-wrap w-[95%] mx-auto">
             <!-- Titulo de la seccion -->
-            <h3 class="w-full text-center text-flamingo font-bold text-2xl mb-4 px-4">Trailer de {{ obra.titulo }}</h3>
+            <h3 class="w-full text-center text-flamingo font-bold text-2xl mb-4 px-4">Trailers</h3>
             <iframe
                 class="w-[100vw] h-auto aspect-video"
                 :src="obra.trailer.trailers"
