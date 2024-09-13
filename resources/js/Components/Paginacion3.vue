d82993578e3dd9b5e6af92fa3bc1391777145f55
Víctor Fernández <victor.fer.gor@proton.me>
2024-09-13 22:29:13 +0200

    mas vistas/componentes adaptadas a las traducciones

--- resources/js/Components/Paginacion3.vue
+++ resources/js/Components/Paginacion3.vue
@@ -12,7 +12,7 @@ const props = defineProps({
             <template v-for="(link) in datos.links">
                 <div v-if="link.url === null" class="m-0 sm:mr-1 sm:mb-1 px-1 py-1.5 sm:px-4 sm:py-3 leading-4 text-gray-500 text-sm sm:text-base" v-html="link.label"/>
                 <Link
-                    v-if="(link.label === '&#38;laquo;Anterior' && datos.current_page !== 1)|| (link.label === 'Siguiente&#38;raquo;' && datos.current_page !== datos.last_page) || link.label === datos.current_page.toString() || (datos.current_page - parseInt(link.label) <= 1 && parseInt(link.label) < datos.current_page) || (parseInt(link.label) - datos.current_page <= 1 && parseInt(link.label) > datos.current_page)"
+                    v-if="((link.label.includes('Anterior') || link.label.includes('Prev')) && datos.current_page !== 1) || ((link.label.includes('Siguiente') || link.label.includes('Next')) && datos.current_page !== datos.last_page) || link.label === datos.current_page.toString() || (datos.current_page - parseInt(link.label) <= 1 && parseInt(link.label) < datos.current_page) || (parseInt(link.label) - datos.current_page <= 1 && parseInt(link.label) > datos.current_page)"
                     as="button"
                     class="m-0 sm:mr-1 sm:mb-1 px-2.5 py-1.5 sm:px-4 sm:py-3 leading-4 hover:bg-flamingo text-sm sm:text-base"
                     :class="{ 'text-white bg-flamingo border-flamingo border-solid border-1 hover:text-black': link.active }"
