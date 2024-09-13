af124904a9061d5274bc1a5a0926db7b55622f9d
Víctor Fernández <victor.fer.gor@proton.me>
2024-09-13 22:49:21 +0200

    mas vistas/componentes adaptadas a las traducciones

--- resources/js/Components/PaginacionSimple.vue
+++ resources/js/Components/PaginacionSimple.vue
@@ -9,18 +9,14 @@ const props = defineProps({
 <template>
     <nav>
         <ul class="flex m-auto">
-            <li v-if="datos.current_page === 1" class="px-4 py-3  md:mr-2 text-gray-600" disabled="true">
-                « Anterior
-            </li>
+            <li v-if="datos.current_page === 1" class="px-4 py-3  md:mr-2 text-gray-600" disabled="true" v-html="$t('pagination.previous')" />
             <li v-else>
-                <Link as="button" class="px-4 py-3 hover:bg-black md:mr-2 hover:text-flamingo font-bold" :href="datos.prev_page_url" v-html="'« Anterior'" preserve-state/>
+                <Link as="button" class="px-4 py-3 hover:bg-black md:mr-2 hover:text-flamingo font-bold" :href="datos.prev_page_url" v-html="$t('pagination.previous')" preserve-state/>
             </li>
             <li v-if="datos.next_page_url">
-                <Link as="button" class="px-4 py-3 hover:bg-black hover:text-flamingo font-bold" :href="datos.next_page_url" v-html="'Siguiente »'" preserve-state/>
+                <Link as="button" class="px-4 py-3 hover:bg-black hover:text-flamingo font-bold" :href="datos.next_page_url" v-html="$t('pagination.next')" preserve-state/>
             </li>
-            <li v-else class="px-4 py-3 mr-2 text-gray-600" disabled="true">
-                Siguiente »
-            </li>
+            <li v-else class="px-4 py-3 mr-2 text-gray-600" disabled="true" v-html="$t('pagination.next')" />
         </ul>
     </nav>
 </template>
