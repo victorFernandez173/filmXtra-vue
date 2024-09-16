981a5d50532d7cddca6d42c8ddcb9e18775f42c0
Víctor Fernández <victor.fer.gor@proton.me>
2024-09-16 12:33:01 +0200

    finalizadas traducciones para pagina error XXX.vue

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -47,8 +47,17 @@ class Handler extends ExceptionHandler
 
         if (!app()->environment(['local', 'testing'])) {
             return match ($status) {
-                404 => Inertia::render('Errors/404')->toResponse($request)->setStatusCode($status),
-                400, 401, 402, 403, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 419, 420,  421, 422, 423, 424, 425, 426, 428, 429, 430, 431, 440, 444, 449, 450, 451, 460, 463, 464, 494, 495, 496, 497, 498, 499, 500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511, 520, 521, 522, 523, 524, 525, 526, 527, 529, 530, 540, 561, 598, 599, 783 => Inertia::render('Errors/XXX', ['status' => $status])->toResponse($request)->setStatusCode($status),
+                404 => Inertia::render('Errors/404', [
+                    'msg' => __('error_404.msg')
+                ])->toResponse($request)->setStatusCode($status),
+                400, 401, 402, 403, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 419, 420,  421, 422, 423, 424, 425, 426, 428, 429, 430, 431, 440, 444, 449, 450, 451, 460, 463, 464, 494, 495, 496, 497, 498, 499, 500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511, 520, 521, 522, 523, 524, 525, 526, 527, 529, 530, 540, 561, 598, 599, 783 => Inertia::render('Errors/XXX',
+                    [
+                        'status' => $status,
+                        'title' => __('error_XXX.title', ['status' => $status]),
+                        'h1' => __('error_XXX.h1', ['status' => $status]),
+                        'msg' => $status == 500 ? __('error_XXX.msg2') : __('error_XXX.msg1'),
+                        'inicio' => __('error_XXX.inicio'),
+                    ])->toResponse($request)->setStatusCode($status),
                 default => $response
             };
         }
--- resources/js/Pages/Errors/XXX.vue
+++ resources/js/Pages/Errors/XXX.vue
@@ -1,36 +1,35 @@
 <script setup>
 import ErrorLayout from "@/Layouts/ErrorLayout.vue";
 import { Head, Link } from "@inertiajs/vue3";
-import { computed } from "vue";
 
 const props = defineProps({
     status: Number,
+    title: String,
+    h1: String,
+    msg: String,
+    inicio: String,
 });
 
-const message = computed(() => {
-    return props.status >= 500 ?
-        'Parece que estamos teniendo problemas.' : 'Parece que ha habido algún error.';
-});
 </script>
 
 <template>
     <Head>
         <title>
-            Error {{ status }}
+            {{ title }}
         </title>
         <meta name="description" content="Página de error">
     </Head>
     <error-layout>
         <h1 class="text-4xl font-bold tracking-tight text-gray-900 md:text-5xl my-5 md:my-10">
-            ERROR {{ status }}
+            {{ h1 }}
         </h1>
         <p class="mb-5 md:mb-10 text-lg md:text-2xl leading-7 text-gray-800 text-center">
-            {{ message }}
+            {{ msg }}
         </p>
         <img src="/error/error-gif.gif" class="m-auto mb-5 md:mb-10 w-80" alt="Gif error cine">
         <div class="flex items-center justify-center">
             <Link :href="route('/')" class="hover:text-flamingo hover:bg-black bg-flamingo px-3.5 py-2.5 text-sm font-semibold text-white">
-                Ir a inicio &rarr;
+                {{ inicio }} &rarr;
             </Link>
         </div>
         <img src="/images/logo.png" class="w-60 my-5 md:my-10" alt="Logo FilmXtra">
