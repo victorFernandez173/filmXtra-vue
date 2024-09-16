00ea88c51c99cb4d3066bf4cc0dad17ebc2dd7b0
Víctor Fernández <victor.fer.gor@proton.me>
2024-09-16 10:26:20 +0200

    finalizadas traducciones en Login.vue y reorganizado fichero json de traducciones

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -63,7 +63,7 @@ const togglePassword = () => {
 <template>
     <Head>
         <title>
-            Login
+            {{ $t('login.title') }}
         </title>
         <meta name="description" content="Página de login">
     </Head>
@@ -77,16 +77,16 @@ const togglePassword = () => {
             <div class="w-[95%] bg-white shadow md:mt-0 sm:max-w-md xl:p-0">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                     <h1 class="text-xl font-bold text-gray-900 md:text-2xl">
-                        Inicia sesión con tu cuenta
+                        {{ $t('login.h1') }}
                     </h1>
                     <!--         Formulario           -->
                     <form class="space-y-4 md:space-y-6" @submit.prevent="submit">
                         <!--             Correo electrónico               -->
                         <div>
-                            <input-label for="email" value="Correo electrónico" class="block mb-2 text-sm font-medium text-gray-900" />
+                            <input-label for="email" :value="$t('login.email_label')" class="block mb-2 text-sm font-medium text-gray-900" />
                             <text-input
                                 id="email"
-                                placeholder="nombre@correo.com"
+                                :placeholder="$t('login.email_label')"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm focus:ring-flamingo  block w-full p-2.5"
                                 v-model="form.email"
                                 autofocus
@@ -96,7 +96,7 @@ const togglePassword = () => {
                         </div>
                         <!--             Contraseña               -->
                         <div>
-                            <input-label for="password" value="Contraseña" class="mb-2 text-sm font-medium text-gray-900 inline-block" />&nbsp;
+                            <input-label for="password" :value="$t('login.password_label')" class="mb-2 text-sm font-medium text-gray-900 inline-block" />&nbsp;
                             <svg id="ojoMostrar" @click="togglePassword" class="inline-block hover:cursor-pointer w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                 <path stroke="currentColor" stroke-width="2" d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z"/>
                                 <path stroke="currentColor" stroke-width="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
@@ -121,13 +121,13 @@ const togglePassword = () => {
                                 :href="route('password.request')"
                                 class="text-sm text-gray-800 hover:text-flamingo focus:outline-none focus:border-[2px] focus:border-flamingo border-[2px] border-white active:text-gray-800"
                             >
-                                ¿Contraseña olvidada?
+                                {{ $t('login.contra_olvidada') }}
                             </Link>
                         </div>
                         <!--botón principal-->
                         <div class="text-center">
                             <primary-button :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                                Inicia sesión
+                                {{ $t('login.inicia_sesion') }}
                             </primary-button>
                         </div>
                         <div class="flex justify-around">
@@ -136,20 +136,20 @@ const togglePassword = () => {
                                 <svg class="w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="github" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
                                     <path fill="currentColor" d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path>
                                 </svg>
-                                Inicia con Github
+                                {{ $t('login.github') }}
                             </a>
                             <!--Iniciar sesion Google-->
                             <a :href="route('auth.google')" type="button" class="text-white bg-[#4285F4] hover:bg-flamingo hover:font-extrabold hover:text-[#4285F4] focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium text-sm px-5 py-2.5 text-center inline-flex items-center">
                                 <svg class="w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
                                     <path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path>
                                 </svg>
-                                Inicia con Google
+                                {{ $t('login.google') }}
                             </a>
                         </div>
                         <p class="text-right text-sm font-light text-gray-700">
-                            ¿Aún no tienes cuenta?
+                            {{ $t('login.tienes_cuenta') }}
                             <Link :href="route('register')" class="font-medium text-primary-600 hover:text-flamingo focus:outline-none focus:border-[2px] focus:border-flamingo border-[2px] border-white">
-                                ¡Regístrate aquí!
+                                {{ $t('login.registrate') }}
                             </Link>
                         </p>
                     </form>
