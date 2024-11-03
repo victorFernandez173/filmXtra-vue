46cc77128d92024a444c4720442595a887539a33
victorFernandez173 <victor.fer.gor@proton.me>
2024-11-03 22:05:44 +0100

    refactoring pint

--- app/Http/Repositorios/CriticasRepo.php
+++ app/Http/Repositorios/CriticasRepo.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Repositorios;
 
-
 use App\Http\Controllers\Controller;
 use App\Models\Critica;
 use Illuminate\Database\Eloquent\Builder;
@@ -12,12 +11,9 @@ class CriticasRepo extends Controller
 {
     /**
      * Crea un array con el contenido, likes y fecha de cada critica para la vista a partir de todas las criticas de la película y devuelve dicha información paginada
-     * @param $criticas
-     * @return _IH_Critica_QB|Builder
      */
-    static function obtenerArrayInfoCriticas($criticas): Builder|_IH_Critica_QB
+    public static function obtenerArrayInfoCriticas($criticas): Builder|_IH_Critica_QB
     {
         return Critica::with(['likes', 'usuario'])->whereIn('id', $criticas->pluck('id'))->withCount('likes');
     }
-
 }
