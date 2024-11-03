24206a3b8d22030be7a6bda51e56f5e2bf27ff5e
victorFernandez173 <victor.fer.gor@proton.me>
2024-11-03 19:46:35 +0100

    refactoring pint

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -12,10 +12,7 @@ class RouteServiceProvider extends ServiceProvider
 {
     /**
      * The path to your application's "home" route.
-     *
      * Typically, users are redirected here after authentication.
-     *
-     * @var string
      */
     public const string HOME = '/';
 
