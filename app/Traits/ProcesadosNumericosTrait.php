b0ee5d68764e94599898f034b1e49e49d368bc7a
victorFernandez173 <victor.fer.gor@proton.me>
2024-11-03 19:11:23 +0100

    refactoring pint

--- app/Traits/ProcesadosNumericosTrait.php
+++ app/Traits/ProcesadosNumericosTrait.php
@@ -3,31 +3,22 @@
 namespace App\Traits;
 
 use App\Models\Obra;
-use Exception;
 
 trait ProcesadosNumericosTrait
 {
-    /**
-     * Retorna int con la decada inicial de las peliculas existentes a partir de la mas antigua
-     * @return int
-     */
-    static function obtenerDecadaPionera(): int
+    public static function obtenerDecadaPionera(): int
     {
-        return (int)(substr(Obra::select(['fecha'])->orderBy('fecha')->first()->fecha, 0, 2).'00');
+        return (int) (substr(Obra::select(['fecha'])->orderBy('fecha')->first()->fecha, 0, 2).'00');
     }
 
-    /**
-     * Retorna array con enumeración de las decadas
-     * @return array
-     * @throws Exception
-     */
-    static function obtenerArrayDecadas(): array
+    public static function obtenerArrayDecadas(): array
     {
         $decadaInicial = static::obtenerDecadaPionera();
         $decadas = [];
-        for ($i = $decadaInicial; $i <= now()->format('Y'); $i+=10) {
+        for ($i = $decadaInicial; $i <= now()->format('Y'); $i += 10) {
             $decadas[] = $i;
         }
+
         return $decadas;
     }
 }
