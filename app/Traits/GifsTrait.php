4fd050f938c9bd66c41544d5fe7a5ad21649fbe8
victorFernandez173 <victor.fer.gor@proton.me>
2024-11-03 19:22:07 +0100

    refactoring pint

--- app/Traits/GifsTrait.php
+++ app/Traits/GifsTrait.php
@@ -7,15 +7,14 @@ use Random\RandomException;
 trait GifsTrait
 {
     /**
-     * Obtenemos el numero de gifs del directorio
-     * y retornamos un int al azar que representa uno de ellos
+     * Obtenemos el numero de gifs del directorio y retornamos un int al azar que representa uno de ellos
      *
-     * @return int
      * @throws RandomException
      */
-    static function obtenerNumDeGif(): int
+    public static function obtenerNumDeGif(): int
     {
         $numDeGifs = count(glob(public_path('/gif/').'*'));
+
         return random_int(1, $numDeGifs);
     }
 }
