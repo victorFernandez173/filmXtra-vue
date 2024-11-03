2c9763c903ecd693a62ed898c5879bc613ef02d8
victorFernandez173 <victor.fer.gor@proton.me>
2024-11-03 19:19:15 +0100

    refactoring pint

--- app/Traits/ProcesadoCamposTrait.php
+++ app/Traits/ProcesadoCamposTrait.php
@@ -2,50 +2,44 @@
 
 namespace App\Traits;
 
-use Exception;
 use Illuminate\Database\Eloquent\Collection;
 
 trait ProcesadoCamposTrait
 {
     /**
      * Retorna string con enumeración de los géneros
-     * @param Collection $generos
-     * @return string
-     * @throws Exception
      */
-    static function procesarGeneros(Collection $generos): string
+    public static function procesarGeneros(Collection $generos): string
     {
         $cadenaGeneros = '';
         $generos->each(function ($valor) use (&$cadenaGeneros) {
-            $cadenaGeneros .= $valor->genero . ', ';
+            $cadenaGeneros .= $valor->genero.', ';
         });
-        return rtrim($cadenaGeneros, ', ') . '.';
+
+        return rtrim($cadenaGeneros, ', ').'.';
     }
 
     /**
      * Retorna string con enumeración los nombres de actores, directores
-     * @param Collection $casting
-     * @return string
-     * @throws Exception
      */
-    static function procesarCasting(Collection $casting): string
+    public static function procesarCasting(Collection $casting): string
     {
         $cadenaCasting = '';
         $casting->each(function ($valor) use (&$cadenaCasting) {
-            $cadenaCasting .= static::procesarNombre($valor->nombre) . ', ';
+            $cadenaCasting .= static::procesarNombre($valor->nombre).', ';
         });
-        return rtrim($cadenaCasting, ', ') . '.';
+
+        return rtrim($cadenaCasting, ', ').'.';
     }
 
     /**
      * Procesa nombres para anteponer nombre al apellido
-     * @param string $nombre
-     * @return string
      */
-    static function procesarNombre(string $nombre): string
+    public static function procesarNombre(string $nombre): string
     {
-        $nombreProcesado = substr($nombre, strrpos($nombre, ',') + 2) . ' ';
+        $nombreProcesado = substr($nombre, strrpos($nombre, ',') + 2).' ';
         $nombreProcesado .= substr($nombre, 0, strrpos($nombre, ','));
+
         return $nombreProcesado;
     }
 }
