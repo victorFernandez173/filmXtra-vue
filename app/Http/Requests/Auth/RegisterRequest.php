f46feba53747ca5239cb579dece492c647815056
victorFernandez173 <victor.fer.gor@proton.me>
2024-11-03 21:50:48 +0100

    refactoring pint

--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -20,12 +20,12 @@ class RegisterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'usuario'  => [
+            'usuario' => [
                 'required',
                 'max:25',
             ],
             // Ahora el bloque de email incorpora una regla que va a tener en cuenta que 'email' es único cuando 'login_tipo_id' es 'filmXtra'. Es decir, es posible que un usuario se pueda registrar con dos cuentas: google y estándar. Así si el usuario ya tiene cuenta para un email con google o'auth y trata de registrarse de manera estándar con el formulario y ese mismo email, podrá: ya que el 'login_tipo_id' no va a coincidir. Pero si ya tiene cuenta estándar no podrá, porque el 'login_tipo_id' si va a coincidir: 'filmXtra'. El usuario podrá tener dos cuentas diferentes si se registra de las dos maneras o incluso más si se registra con diferentes proveedores o'auth: google y github.
-            'email'    => [
+            'email' => [
                 'required',
                 'max:60',
                 'email:rfc,dns', Rule::unique('usuarios', 'email')->where('login_tipo_id', LoginTipo::FILMXTRA_TIPO),
@@ -44,7 +44,7 @@ class RegisterRequest extends FormRequest
     public function attributes(): array
     {
         return [
-            'usuario'  => trans('validation.attributes.usuario'),
+            'usuario' => trans('validation.attributes.usuario'),
             'email' => trans('validation.attributes.email'),
             'password' => trans('validation.attributes.password'),
         ];
