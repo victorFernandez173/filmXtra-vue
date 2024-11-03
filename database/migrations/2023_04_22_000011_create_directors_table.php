1ed9ad907477940dc8d37ef6288e8bfc15e830ab
victorFernandez173 <victor.fer.gor@proton.me>
2024-11-03 19:06:44 +0100

    refactoring pint

--- database/migrations/2023_04_22_000011_create_directors_table.php
+++ database/migrations/2023_04_22_000011_create_directors_table.php
@@ -1,9 +1,9 @@
 <?php
 
+use App\Models\Director;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Models\Director;
 
 return new class extends Migration
 {
