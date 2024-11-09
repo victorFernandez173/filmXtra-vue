2e8de36f5a000d703f04cef66e30387224af1aea
victorFernandez173 <victor.fer.gor@proton.me>
2024-11-09 13:21:04 +0100

    nuevo parametro user para las vistas de error que por defecto inertia no pasa debido a la inclusión de las paginas de error en el layout general

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -30,12 +30,14 @@ class Handler extends ExceptionHandler
         $status = $response->getStatusCode();
 
         if (! app()->environment(['local', 'testing'])) {
+
             return match ($status) {
                 404 => Inertia::render('Errors/404',
                     [
                         'title' => __('error_404.title'),
                         'msg' => __('error_404.msg'),
                         'inicio' => __('error_404.inicio'),
+                        'auth' => auth()->check() ? ['user' => auth()->user()] : ['user' => null],
                     ]
                 )->toResponse($request)->setStatusCode($status),
                 400, 401, 402, 403, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 419, 420,  421, 422, 423, 424, 425, 426, 428, 429, 430, 431, 440, 444, 449, 450, 451, 460, 463, 464, 494, 495, 496, 497, 498, 499, 500, 501, 502, 503, 504, 505, 506, 507, 508, 509, 510, 511, 520, 521, 522, 523, 524, 525, 526, 527, 529, 530, 540, 561, 598, 599, 783 => Inertia::render('Errors/XXX',
@@ -45,6 +47,7 @@ class Handler extends ExceptionHandler
                         'h1' => __('error_XXX.h1', ['status' => $status]),
                         'msg' => $status == 500 ? __('error_XXX.msg2') : __('error_XXX.msg1'),
                         'inicio' => __('error_XXX.inicio'),
+                        'auth' => auth()->check() ? ['user' => auth()->user()] : ['user' => null],
                     ]
                 )->toResponse($request)->setStatusCode($status),
                 default => $response,
